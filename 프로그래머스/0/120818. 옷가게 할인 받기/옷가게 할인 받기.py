38f09faf0965a20525b82d2ce94f8b24b9ed4c3b
Rorarion <wnsghcoswp@gmail.com>
2024-12-17 11:56:13 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.py
@@ -1,3 +1,11 @@
 def solution(price):
     answer = 0
+    if price>= 500000 :
+        answer = int(price*0.80)
+    elif 500000> price >=300000 :
+        answer = int(price*0.90)
+    elif 300000> price >=100000 :
+        answer = int(price*0.95)
+    else :
+        answer = int(price)
     return answer
