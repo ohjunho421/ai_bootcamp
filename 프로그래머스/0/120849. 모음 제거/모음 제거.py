7970bad723e4fb5032a0aa2ed25bb356e47d02c2
Rorarion <wnsghcoswp@gmail.com>
2024-12-19 09:04:21 +0900

    [level 0] Title: 모음 제거, Time: 0.01 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.py
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.py
@@ -1,4 +1,3 @@
 def solution(my_string):
-    remove_char = 'aeiou'
-    answer = my_string.translate(str.maketrans("","",remove_char))
+    answer = my_string.translate({ord(letter): None for letter in 'aeiou'})
     return answer
