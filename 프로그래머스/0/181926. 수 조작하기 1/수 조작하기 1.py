05d5b435dabd7811123acd7c28debf2e240a7491
Rorarion <wnsghcoswp@gmail.com>
2024-12-23 09:54:04 +0900

    [level 0] Title: 수 조작하기 1, Time: 14.05 ms, Memory: 10.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,15 @@
+def solution(n, control):
+    answer = n
+
+    control = list(str(control))
+    for char in control:
+        if char == 'w':
+            answer += 1
+        elif char == 's':
+            answer -= 1
+        elif char == 'd':
+            answer += 10
+        elif char == 'a':
+            answer -= 10
+
+    return answer
